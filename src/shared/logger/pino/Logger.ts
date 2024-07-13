2da29424e76d8a787d345a031c02746080306dd7
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-07-13 12:12:01 -0300

    Refatoração da implementação do logger com o Pino

--- src/shared/logger/pino/Logger.ts
+++ src/shared/logger/pino/Logger.ts
@@ -5,11 +5,25 @@ const pinoLogger = pino({
   level: 'debug',
 })
 
+const parseLoggerInputToPinoFormat = <T>({
+  message,
+  error,
+  ...data
+}: LogData<T>) => ({
+  msg: message,
+  err: error,
+  ...(data?.payload ? data.payload : data),
+})
+
 const AppLogger: Logger = {
-  debug: <T>(logData: LogData<T>) => pinoLogger.debug(logData),
-  info: <T>(logData: LogData<T>) => pinoLogger.info(logData),
-  warn: <T>(logData: LogData<T>) => pinoLogger.warn(logData),
-  error: <T>(logData: LogData<T>) => pinoLogger.error(logData),
+  debug: <T>(logData: LogData<T>) =>
+    pinoLogger.debug(parseLoggerInputToPinoFormat(logData)),
+  info: <T>(logData: LogData<T>) =>
+    pinoLogger.info(parseLoggerInputToPinoFormat(logData)),
+  warn: <T>(logData: LogData<T>) =>
+    pinoLogger.warn(parseLoggerInputToPinoFormat(logData)),
+  error: <T>(logData: LogData<T>) =>
+    pinoLogger.error(parseLoggerInputToPinoFormat(logData)),
 }
 
 export default (): Logger => AppLogger
