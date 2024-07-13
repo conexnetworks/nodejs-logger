19a35c7c578a9f94402d578d31ad6500673b177a
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-07-13 15:26:59 -0300

    Registrando requisições HTTP nos logs

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -3,34 +3,50 @@ import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
 import { errors } from 'celebrate'
+import pinoHttp from 'pino-http'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
 import swaggerFile from '../../swagger.json'
 import '@shared/container'
 import uploadConfig from '@config/upload'
 import { makeLogger } from '../logger/pino/index'
+import { fileTransport } from '@shared/logger/pino/Logger'
 
 export const logger = makeLogger()
 
-const data = {
-  user: {
-    id: 'userid',
-    name: 'John Doe',
-    email: 'a@a.com',
-    password: '1234',
-    created_at: new Date(),
-  },
-}
+// const data = {
+//   user: {
+//     id: 'userid',
+//     name: 'John Doe',
+//     email: 'a@a.com',
+//     password: '1234',
+//     created_at: new Date(),
+//   },
+// }
 
-logger.info<typeof data>({
-  type: 'Info',
-  message: 'User info',
-  payload: data,
-})
+// logger.info<typeof data>({
+//   type: 'Info',
+//   message: 'User info',
+//   payload: data,
+// })
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use(
+  pinoHttp(
+    {
+      autoLogging: true,
+      redact: {
+        paths: ['req.headers.authorization'],
+        remove: true,
+      },
+    },
+    fileTransport,
+  ),
+)
+
 app.use('/files', express.static(uploadConfig.directory))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
